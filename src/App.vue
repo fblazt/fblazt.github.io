71c9a86904fcb128ae3037ddf4131d04c9e320d9
Firman <fblazt@outlook.com>
2024-06-15 15:55:31 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen w-full bg-black flex items-center justify-center">
+    <a class="font-light text-3xl text-white" href="https://www.github.com/fblazt">fblazt</a>
+  </div>
+</template>
