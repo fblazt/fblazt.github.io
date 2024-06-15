ebc8eacd12ca7b7c4e9db2962ffc93b2b35f1c27
Firman <fblazt@outlook.com>
2024-06-15 16:21:48 +0700

    Revert font style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="h-screen w-full bg-black flex items-center justify-center">
-    <a class="font-light text-3xl text-green-400" href="https://www.github.com/fblazt">fblazt</a>
+    <a class="font-light text-3xl text-white" href="https://www.github.com/fblazt">fblazt</a>
   </div>
 </template>
